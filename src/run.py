71463f89ba517d9debeefe4779d28404ee8042f7
ronan <ronand1997@gmail.com>
2022-05-02 18:54:34 +0100

    init functionality with different sds011 pkg

--- src/run.py
+++ src/run.py
@@ -1,71 +1,61 @@
-import serial, struct, sys, time, json, subprocess
+from sds011_particle_sensor.Code.sds011 import SDS011
 from dotenv import load_dotenv
 import os
+import time
 
-class AirQualitySensor:
+
+load_dotenv()
+ENVIRONMENT = os.environ['ENVIRONMENT']
+DEBUG = os.environ['DEBUG']
+CYCLES = os.environ['CYCLES']
+TIMEOUT = os.environ['TIMEOUT']
+SERIAL_PATH = os.environ['SERIAL_PATH']
+
+class Measurements:
+
     def __init__(self) -> None:
-        self.ser = None
-        self.sleep_s = 1
-        load_dotenv()
-        self.DEBUG = os.getenv("DEBUG")
-        CMD_MODE = os.getenv("CMD_MODE")
-        CMD_QUERY_DATA = os.getenv("CMD_QUERY_DATA")
-        CMD_DEVICE_ID = os.getenv("CMD_DEVICE_ID")
-        self.CMD_SLEEP = os.getenv("CMD_SLEEP")
-        CMD_FIRMWARE = os.getenv("CMD_FIRMWARE")
-        CMD_WORKING_PERIOD = os.getenv("CMD_WORKING_PERIOD")
-        MODE_ACTIVE = os.getenv("MODE_ACTIVE")
-        MODE_QUERY = os.getenv("MODE_QUERY")
-        PERIOD_CONTINUOUS = os.getenv("PERIOD_CONTINUOUS")
+        self.unit = SDS011.UnitsOfMeasure.MassConcentrationEuropean
+        self.sensor = SDS011(
+            device_path=SERIAL_PATH,
+            unit_of_measure=self.unit,
+            timeout=TIMEOUT,
+        )
 
-    def __enter__(self):
-        return self
+    def get_sensor_info(self) -> None:
+        print(f"""
+        SDS011 Sensor Info.
+        Device ID: {self.sensor.device_id}
+        Device Firmware: {self.sensor.firmware}
+        Current device cycle (0 is permanent on): {self.sensor.dutycycle}
+        Sensor Workstate: {self.sensor.workstate}
+        Sensor Report Mode: {self.sensor.reportmode}
 
-    def __exit__(self, *args, **kwargs):
-        pass
+        Number of measurements in permanent measuring mode: {CYCLES * 2}
+        """)
 
-    def _initiate_serializer(self):
-        """
-        Initiate connection to USB serial port.
-        """
-        self.ser = serial.Serial()
-        self.ser.port = "/dev/ttyUSB0"
-        self.ser.baudrate = 9600  # speed of communication over channel
-        self.ser.open()
-        self.ser.flushInput()
+    def _sleep_sensor(self) -> None:
+        self.sensor.workstate = self.sensor.WorkStates.Sleeping
 
-    def cmd_set_sleep(self):
-        self.ser.write(self.construct_command([0x1, 1]))
-        response = self.read_response()
-
-    def read_response(self):
-        byte = 0
-        while byte != "\xaa":
-            byte = self.ser.read(size=1)
-
-        d = self.ser.read(size=9)
-
-        if self.DEBUG:
-            self.dump(d, '< ')
-        return byte + d
-
-    def construct_command(self, cmd, data=[]):
-        assert len(data) <= 12
-        data += [0,]*(12-len(data))
-        checksum = (sum(data)+self.CMD_SLEEP-2)%256
-        ret = "\xaa\xb4" + chr(self.CMD_SLEEP)
-        ret += ''.join(chr(x) for x in data)
-        ret += "\xff\xff" + chr(checksum) + "\xab"
-
-        if self.DEBUG:
-            self.dump(ret, '> ')
-        return ret
-
-    def dump(d, prefix=''):
-        print(prefix + ' '.join(x.encode('hex') for x in d))
+    def _take_measurements(self) -> None:
+        self.sensor.reset()
+        for cycle in CYCLES:
+            values = self.sensor.get_values()
+            if values is not None:
+                self.print_values(0, values, self.unit)
+    
+    @staticmethod
+    def print_values(timing, values, unit_of_measure):
+        if unit_of_measure == SDS011.UnitsOfMeasure.MassConcentrationEuropean:
+            unit = 'µg/m³'
+        else:
+            unit = 'pcs/0.01cft'
+        print("Waited %d secs\nValues measured in %s:    PM2.5  " %
+            (timing, unit), values[1], ", PM10 ", values[0])
 
 if __name__ == "__main__":
-    with AirQualitySensor() as aqi:
-        print(dir(aqi.ser))
-        aqi.cmd_set_sleep()
-    
+
+    meas = Measurements()
+    meas.get_sensor_info()
+    meas._take_measurements()
+
+    pass
